bf37dfcb921ff075da26abba4c6a5f28c14e9ca0
EvgHrn <evg.hrn@gmail.com>
2018-10-01 12:00:27 +0400

    Second temp sensor

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,6 +1,6 @@
 // библиотека для работы с GPRS устройством
 #include <GPRS_Shield_Arduino.h>
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 #include "DHT.h"
 
 // длина сообщения
@@ -9,6 +9,10 @@
 #define DHTPIN 12
 
 #define DHTTYPE DHT21
+
+#define DHTPIN2 13
+
+#define DHTTYPE2 DHT22
 
 // номер на который будем отправлять сообщение
 #define PHONE_NUMBER  "+79090520560"
@@ -26,28 +30,33 @@ char datetime[24];
 
 
 DHT dht(DHTPIN, DHTTYPE);
+DHT dht2(DHTPIN2, DHTTYPE2);
 
-SoftwareSerial gprsSerial(10, 11);
+//SoftwareSerial gprsSerial(10, 11);
  
 // создаём объект класса GPRS и передаём в него объект Serial1 
 //GPRS gprs(gprsSerial);
 // можно указать дополнительные параметры — пины PK и ST
 // по умолчанию: PK = 2, ST = 3
-GPRS gprs(gprsSerial, 2, 3);
+GPRS gprs(Serial);
+GPRS gprs2(Serial);
  
 void setup()
 {
+//  pinMode(A5, INPUT_PULLUP);
   // открываем последовательный порт для мониторинга действий в программе
   Serial.begin(9600);
+  
   // ждём пока не откроется монитор последовательного порта
   // для того, чтобы отследить все события в программе
   while (!Serial) {
   }
-  Serial.print("Serial init OK\r\n");
+  //Serial.print("Serial init OK\r\n");
 
   dht.begin();
+  dht2.begin();
   // открываем Serial-соединение с GPRS Shield
-  gprsSerial.begin(9600);
+//  gprsSerial.begin(9600);
   // включаем GPRS-шилд
   gprs.powerOn();
   // проверяем, есть ли связь с GPRS-устройством
@@ -57,11 +66,11 @@ void setup()
     // процесс повторяется в цикле,
     // пока не появится ответ от GPRS-устройства
     delay(1000);
-    Serial.print("GPRS Init error\r\n");
+    //Serial.print("GPRS Init error\r\n");
   }
   // выводим сообщение об удачной инициализации GPRS Shield
-  Serial.println("GPRS init success");
-  Serial.println("Please send SMS message to me!");
+  //Serial.println("GPRS init success");
+  //Serial.println("Please send SMS message to me!");
 }
  
 void loop()
@@ -70,27 +79,34 @@ void loop()
   if (gprs.ifSMSNow()) {
     // читаем его
     gprs.readSMS(smsmessage, phone, datetime);
-    Serial.println(smsmessage);
+    //Serial.println(smsmessage);
 
     if(strcmp(smsmessage, "0") == 0) {
 
       //read temperature from dht21
       delay(1000);
-      float t = dht.readTemperature();
-    
+      float t1 = dht.readTemperature();
+      delay(1000);
+      float t2 = dht2.readTemperature();
       // Check if any reads failed and exit early (to try again).
-      if (isnan(t)) {
-        Serial.println("Failed to read from DHT sensor!");
-        return;
+      if (isnan(t1)) {
+        //Serial.println("Failed to read from DHT sensor!");
+        t1 = 99.99;
       }
 
+      if (isnan(t2)) {
+        //Serial.println("Failed to read from DHT sensor!");
+        t2 = 99.99;
+      }
 
       //convert float temperature to char array for sending
-      String messageObj = String (t);
-      char message[5];
-      messageObj.toCharArray(message, 5);
+      String messageObj = String ("t1: ") + String (t1) + String (" t2: ") + String (t2);
+      char message[20];
+      messageObj.toCharArray(message, 20);
 
-      //send sms with temperature
+//      Serial.print(message);
+
+//      send sms with temperature
       gprs.sendSMS(PHONE_NUMBER, message);
     }
   }
