7d5de1de937e7a249e26d786964c0bdb9ccd9ed9
EvgHrn <evg.hrn@gmail.com>
2019-01-31 14:50:06 +0400

    Add dotenv with secret data

--- reactApp/App.js
+++ reactApp/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PouchDB from 'pouchdb';
+
+let remote_db = new PouchDB(`http://${process.env.DB_LOG}:${process.env.DB_PASS}@${process.env.DB_IP}:${process.env.DB_PORT}/smarthome`);
+let local_db = new PouchDB('smarthome');
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Date</Text>
+        <Text>Temperature</Text>
+        <Text>Humidity</Text>
       </View>
     );
   }
