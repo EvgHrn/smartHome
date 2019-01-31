23611c255bd3060cd495b13e5fbea400792cac74
EvgHrn <evg.hrn@gmail.com>
2019-01-31 15:34:18 +0400

    Add sync

--- reactApp/App.js
+++ reactApp/App.js
@@ -2,10 +2,30 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PouchDB from 'pouchdb';
 
+require('dotenv').config();
+
 let remote_db = new PouchDB(`http://${process.env.DB_LOG}:${process.env.DB_PASS}@${process.env.DB_IP}:${process.env.DB_PORT}/smarthome`);
-let local_db = new PouchDB('smarthome');
+let local_db = new PouchDB('smarthomeDb');
 
 export default class App extends React.Component {
+  getDerivedStateFromProps(props, state) {
+    var sync = PouchDB.sync(local_db, remote_db, {
+      live: true,
+      retry: true
+    }).on('change', function (info) {
+      // handle change
+    }).on('paused', function (err) {
+      // replication paused (e.g. replication up to date, user went offline)
+    }).on('active', function () {
+      // replicate resumed (e.g. new changes replicating, user went back online)
+    }).on('denied', function (err) {
+      // a document failed to replicate (e.g. due to permissions)
+    }).on('complete', function (info) {
+      // handle complete
+    }).on('error', function (err) {
+      // handle error
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
