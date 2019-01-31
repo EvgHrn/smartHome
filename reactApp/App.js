2be3176528931acfeadf6e774bd0f86ec7249992
EvgHrn <evg.hrn@gmail.com>
2019-01-31 16:26:05 +0400

    Add pouchdb find plugin

--- reactApp/App.js
+++ reactApp/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PouchDB from 'pouchdb';
+PouchDB.plugin(require('pouchdb-find'));
 
 require('dotenv').config();
 
